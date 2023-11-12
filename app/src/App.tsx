142060b74056bee06496b5b72dc375a1183544d8
devlopersabbir <devlopersabbir@gmail.com>
2023-11-12 16:51:39 +0600

    creating sidebar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,17 @@
-import { Box, Center, Container, Flex, HStack } from "@chakra-ui/react";
 import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <div>
-        {/* side menu */}
-        <div>side menu</div>
-        {/* sidebar */}
-        <div>side bar</div>
-        {/* chat box */}
-        <div>chat box</div>
+    <div className="m-10">
+      <div className="flex justify-center items-center bg-white w-full h-[87vh]">
+        <div className="flex border-2 bg-gray-900 rounded-4xl w-full">
+          {/* side menu */}
+          <div className="flex w-32 h-[87vh] bg-zinc-200">side menu</div>
+          {/* sidebar */}
+          <div className="flex">side bar</div>
+          {/* chat box */}
+          <div className="flex">chat box</div>
+        </div>
       </div>
     </div>
   );
