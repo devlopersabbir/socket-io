e15e509ff4e08d531960ffeab4f4f24e5cdea823
devlopersabbir <devlopersabbir@gmail.com>
2023-11-13 16:47:37 +0600

    side menu structure ready

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,14 +3,22 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div className="m-10">
-      <div className="flex justify-center items-center bg-white w-full h-[87vh]">
-        <div className="flex border-2 bg-gray-900 rounded-4xl w-full">
+      <div className="flex justify-center items-center bg-white w-full h-[87vh] rounded-3xl">
+        <div className="flex rounded-3xl w-full shadow-2xl backdrop-blur-3xl">
           {/* side menu */}
-          <div className="flex w-32 h-[87vh] bg-zinc-200">side menu</div>
+          <div className="flex flex-col gap-5 justify-center items-center text-center w-24 rounded-bl-3xl rounded-tl-3xl bg-zinc-200 p-5">
+            <div className="logo w-10 h-10 rounded-md bg-blue-600"></div>
+            <div className="footer flex flex-col">
+              {/* mode */}
+              {/* profile photo */}
+            </div>
+          </div>
           {/* sidebar */}
-          <div className="flex">side bar</div>
+          <div className="flex w-72 bg-zinc-50">side bar</div>
           {/* chat box */}
-          <div className="flex">chat box</div>
+          <div className="flex rounded-br-3xl rounded-tr-3xl w-full h-[87vh] bg-slate-100">
+            chat box
+          </div>
         </div>
       </div>
     </div>
